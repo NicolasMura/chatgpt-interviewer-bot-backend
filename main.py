b7bd76be0723d6602d8d85e8acf5370739d6fc86
NicolasMura <contact@nicolasmura.fr>
2024-07-14 15:22:37 +0200

    Update base URL

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 origins = [
-    "http://localhost:5174",
-    "http://localhost:5173",
-    "http://localhost:8000",
-    "http://localhost:8001",
-    "http://localhost:3000",
+    # "http://localhost:5174",
+    # "http://localhost:5173",
+    # "http://localhost:8000",
+    # "http://localhost:8001",
+    # "https://chatgpt-interviewer-bot-backend.onrender.com",
 ]
 
 app.add_middleware(
