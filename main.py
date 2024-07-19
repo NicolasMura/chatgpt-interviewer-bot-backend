fd6cb28b74ef50dae511a5216ba27b4958e67a80
NicolasMura <contact@nicolasmura.fr>
2024-07-19 11:56:15 +0200

    Debug with minimalist main.py

--- main.py
+++ main.py
@@ -13,4 +13,32 @@ from openai.types.audio import Transcription
 from openai.types.chat import ChatCompletion
 
 load_dotenv()
+
+elevenlabs_key = os.getenv("ELEVENLABS_KEY")
 print('OPEN_AI_KEY: ', os.getenv("OPEN_AI_KEY"))
+print('elevenlabs_key: ', elevenlabs_key)
+
+app = FastAPI()
+app.mount("/static/browser",
+          StaticFiles(directory="static/browser"), name="static")
+
+origins = [
+    # "http://localhost:5174",
+    # "http://localhost:5173",
+    # "http://localhost:8000",
+    # "http://localhost:8001",
+    "https://chatgpt-interviewer-bot-backend.onrender.com",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    return FileResponse("static/browser/index.html")
